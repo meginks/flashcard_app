26f689266f511e26741268b6f828b993fe18786e
meginks <meganjones.dev@gmail.com>
2020-02-09 19:56:50 -0800

    created react app and built out file structure

---
+++ flashcards/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Flashcards /> 
+    </div>
+  );
+}
+
+export default App;
