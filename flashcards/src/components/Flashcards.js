ec5bc7e922101ee940c28f46d8dcecc3f75b7f95
meginks <meganjones.dev@gmail.com>
2020-02-10 18:23:50 -0800

    added axios call

--- flashcards/src/components/Flashcards.js
+++ flashcards/src/components/Flashcards.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+import axios from 'axios';
 
 class Flashcards extends React.Component {
+    state = { 
+        nouns = []
+    }
+    componentDidMount() {
+        axios.get('localhost:4001/parole/nouns').then(res => {
+            this.setState({nouns: res.data}).catch(error => console.log(error))
+        })
+    }
 
+    render() {
+        return (
+        <Container>
+            
+        </Container>
+        )
+        
+    }
 } 
 
 
